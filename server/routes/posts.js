85e057df9b14de302daeadb1ed17b07d155994c8
ebssgd <iamebssgd@yahoo.com>
2021-10-06 16:00:28 -0500

    Added route for photos

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -57,7 +57,7 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-//TODO: Get Posts
+//TODO: Get Post
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
