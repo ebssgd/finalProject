e6a234bb0a40caef4bbe7dc257ab17b4f47458f0
ebssgd <iamebssgd@yahoo.com>
2021-10-06 22:42:45 -0500

    Added image funcitonality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,9 +7,9 @@ router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    return res.status(200).json(savedPost);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
